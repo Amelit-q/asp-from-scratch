e06034d932ce4458a2df24cb20c7abed60b6246d
Amelit <muradovt111@gmail.com>
2021-11-18 21:53:43 +0300

    controllers was moved into new folder

---
+++ Controllers/MeetupController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspFromScratch.WebApi.Controllers;
+
+[ApiController]
+[Route("/meetups")]
+public class MeetupController : ControllerBase
+{
+    private static readonly ICollection<Meetup> Meetups = new List<Meetup>();
+
+    public class Meetup
+    {
+        public Guid? Id { get; set; }
+        public string Topic { get; set; }
+        public string Place { get; set; }
+        public int Duration { get; set; }
+    }
+
+
+    [HttpPut("{id:guid}")]
+    public IActionResult UpdateMeetup([FromRoute] Guid id, [FromBody] Meetup updatedMeetup)
+    {
+        var prevMeetup = Meetups.SingleOrDefault(meetup => meetup.Id == id);
+
+        if (prevMeetup is null)
+        {
+            return NotFound();
+        }
+
+        prevMeetup.Topic = updatedMeetup.Topic;
+        prevMeetup.Duration = updatedMeetup.Duration;
+        prevMeetup.Place = updatedMeetup.Place;
+
+        return NoContent();
+    }
+
+    [HttpGet]
+    public IActionResult GetMeetups()
+    {
+        return Ok(Meetups);
+    }
+
+    [HttpDelete]
+    public IActionResult DeleteMeetup([FromRoute] Guid id)
+    {
+        var meetupToDelete = Meetups.SingleOrDefault(meetup => meetup.Id == id);
+
+        if (meetupToDelete is null)
+        {
+            return NotFound();
+        }
+
+        Meetups.Remove(meetupToDelete);
+        return Ok(meetupToDelete);
+    }
+
+    [HttpPost]
+    public IActionResult CreateMeetup([FromBody] Meetup newMeetup)
+    {
+        newMeetup.Id = Guid.NewGuid();
+        Meetups.Add(newMeetup);
+        return Ok(newMeetup);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,54 +16,5 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var meetups = new List<Meetup>();
-
-app.MapGet("/get-string", () => Results.Ok("it's ok this is the way how it suppose to works"));
-
-app.MapGet("/meetups", () => Results.Ok(meetups));
-
-app.MapDelete("/meetups/{id:guid}", ([FromRoute] Guid id) =>
-{
-    var meetupToDelete = meetups.SingleOrDefault(meetup => meetup.Id == id);
-
-    if (meetupToDelete is null)
-    {
-        return Results.NotFound();
-    }
-
-    meetups.Remove(meetupToDelete);
-    return Results.Ok(meetupToDelete);
-});
-
-app.MapPost("/meetups", ([FromBody] Meetup newMeetup) =>
-{
-    newMeetup.Id = Guid.NewGuid();
-    meetups.Add(newMeetup);
-    return Results.Ok(newMeetup);
-});
-
-app.MapPut("/meetups/{id:guid}", ([FromRoute] Guid id, [FromBody] Meetup updatedMeetup) =>
-{
-    var prevMeetup = meetups.SingleOrDefault(meetup => meetup.Id == id);
-
-    if (prevMeetup is null)
-    {
-        return Results.NotFound();
-    }
-
-    prevMeetup.Topic = updatedMeetup.Topic;
-    prevMeetup.Duration = updatedMeetup.Duration;
-    prevMeetup.Place = updatedMeetup.Place;
-
-    return Results.NoContent();
-});
 
 app.Run();
-
-class Meetup
-{
-    public Guid? Id { get; set; }
-    public string Topic { get; set; }
-    public string Place { get; set; }
-    public int Duration { get; set; }
-}
