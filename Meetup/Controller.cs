30b12493f27cf5797532f106ec9efde05d0904c8
Amelit <muradovt111@gmail.com>
2021-11-21 18:46:03 +0300

    added swagger documentation for methods and DTO

--- Meetup/Controller.cs
+++ Meetup/Controller.cs
@@ -9,7 +9,10 @@ public class MeetupController : ControllerBase
     private static readonly ICollection<Meetup> Meetups = new List<Meetup>();
 
 
+    /// <summary>Creates new meetup</summary>
+    /// <response code="200">Meetup was created</response>
     [HttpPost]
+    [ProducesResponseType(typeof(ReadMeetupDto), StatusCodes.Status200OK)]
     public IActionResult CreateMeetup([FromBody] CreateMeetupDto createDto)
     {
         var newMeetup = new Meetup
@@ -31,7 +34,13 @@ public class MeetupController : ControllerBase
         return Ok(readDto);
     }
 
+    /// <summary>Update information about meetup</summary>
+    /// <param name="id" example="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx">Update meetup by ID</param>
+    /// <response code="200">Update information about meetup</response>
+    /// <response code="404">Meetup was not found</response>
     [HttpPut("{id:guid}")]
+    [ProducesResponseType(typeof(ReadMeetupDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult UpdateMeetup([FromRoute] Guid id, [FromBody] UpdateMeetupDto updateDto)
     {
         var prevMeetup = Meetups.SingleOrDefault(meetup => meetup.Id == id);
@@ -48,7 +57,12 @@ public class MeetupController : ControllerBase
         return NoContent();
     }
 
+    /// <summary>Get all meetups</summary>
+    /// <response code="200">List of the meetups</response>
+    /// <response code="404">Meetups was not found</response>
     [HttpGet]
+    [ProducesResponseType(typeof(ReadMeetupDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult GetMeetups()
     {
         var readDtos = Meetups.Select(meetups => new ReadMeetupDto
@@ -62,7 +76,13 @@ public class MeetupController : ControllerBase
         return Ok(readDtos);
     }
 
+    /// <summary>Delete meetup by id</summary>
+    /// <param name="id" example="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx">Meetup Id which suppose to be deleted</param>
+    /// <response code="200">Meetup was successfully deleted</response>
+    /// <response code="400">Meetup was not found</response>
     [HttpDelete("{id:guid}")]
+    [ProducesResponseType(typeof(ReadMeetupDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult DeleteMeetup([FromRoute] Guid id)
     {
         var meetupToDelete = Meetups.SingleOrDefault(meetup => meetup.Id == id);
--- Meetup/Model.cs
+++ Meetup/Model.cs
@@ -2,8 +2,19 @@
 
 internal class Meetup
 {
+    /// <summary>Meetup.id</summary>
+    /// <example>xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx</example>
     public Guid Id { get; set; }
+
+    /// <summary>Topic discussed on meetup</summary>
+    /// <example>Microsoft naming issues</example>
     public string Topic { get; set; }
+
+    /// <summary>Meetup location</summary>
+    /// <example>Oslo</example>
     public string Place { get; set; }
+
+    /// <summary>Meetup duration in minutes</summary>
+    /// <example>180</example>
     public int Duration { get; set; }
 }
