1bf39ede6a438fd5cff971d2e67537c5b94a9dcb
Amelit <muradovt111@gmail.com>
2021-12-01 21:55:33 +0300

    added new api controller method and context

--- User/Controller.cs
+++ User/Controller.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AspFromScratch.WebApi.User;
 
@@ -8,11 +12,16 @@ namespace AspFromScratch.WebApi.User;
 public class UserController : ControllerBase
 {
     private readonly DatabaseContext _context;
+    private readonly IConfiguration _configuration;
 
-    public UserController(DatabaseContext context) =>
+    public UserController(DatabaseContext context, IConfiguration configuration)
+    {
         _context = context;
+        _configuration = configuration;
+    }
 
-    [HttpPost]
+
+    [HttpPost("register")]
     [ProducesResponseType(typeof(ReadUserDto), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status409Conflict)]
     public async Task<IActionResult> RegisterNewUser([FromBody] RegisterUserDto registerDto)
@@ -43,5 +52,33 @@ public class UserController : ControllerBase
             Username = newUser.Username
         };
         return Ok(readDto);
+    }
+
+    public async Task<IActionResult> AuthenticateUser([FromBody] AuthenticateUserDto authenticateDto)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(user => user.Username == authenticateDto.Username);
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        if (BCrypt.Net.BCrypt.Verify(authenticateDto.Password, user.Password))
+        {
+            return Conflict("Incorrect password");
+        }
+
+        // Prepare token info
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var jwtSecret = Encoding.ASCII.GetBytes(_configuration["JwtAuth:Secret"]);
+        var accessTokenLifetime = int.Parse(_configuration["JwtAuth:AccessTokenLifetime"]);
+        var refreshTokenLifetime = int.Parse(_configuration["JwtAuth:RefreshTokenLifetime"]);
+
+        var accessTokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[] { new Claim("sub", user.Id.ToString()) }),
+            Expires = DateTime.UtcNow.AddMinutes(accessTokenLifetime),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(jwtSecret),
+                SecurityAlgorithms.HmacSha256Signature)
+        };
     }
 }
